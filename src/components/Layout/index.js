096cd145be79a5a73ece0a932c4d6078aba97aa8
Robin Solanki <web.guru@me.com>
2020-04-28 18:35:49 +0530

    Create custom hook to get siteMetadata

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,18 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
 
+//Hooks
+
+import { useMetaDataQuery } from "hooks/useMetaDataQuery"
+
 // Styles
 import { GlobalStyles, lightTheme, darkTheme } from "styles/GlobalStyles"
 
 const Layout = ({ children }) => {
+  const data = useMetaDataQuery()
+
+  console.log(data)
+
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
