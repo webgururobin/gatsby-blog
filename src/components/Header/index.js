ce605f919cff558a15cc0611cf5fecf13bf267c8
Robin Solanki <web.guru@me.com>
2020-04-29 13:43:43 +0530

    Configure Mobile Menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 // Components
 import Menu from "components/Menu"
 import Hamburger from "components/Hamburger"
+import MobileMenu from "components/MobileMenu"
 
 // Hooks
 import { useSiteConfigQuery } from "hooks/useSiteConfigQuery"
@@ -19,6 +20,7 @@ const Header = ({ siteTite = `` }) => {
   return (
     <Wrapper>
       <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} items={siteConfig.menu} />
       <Menu items={siteConfig.menu} />
       <Link to="/">
         <Logo src={siteConfig.logo.publicURL} alt={siteTite} />
