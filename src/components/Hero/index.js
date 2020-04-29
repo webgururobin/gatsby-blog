05a957c13102a451b038c10533317e665fbd2463
Robin Solanki <web.guru@me.com>
2020-04-29 14:32:24 +0530

    Add Hero Component Structure

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,26 @@
 import React from "react"
+import { Link } from "gatsby"
+
+// Component
+
+// Hooks
 
 // Styles
 
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <div>
+      <div>
+        BG image
+        <div>
+          <h2>Title</h2>
+          <Link to="/">
+            <button>Click me</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
