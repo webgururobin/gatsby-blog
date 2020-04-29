62009c20384500b4b6dfca9513cb2c17615f5afc
Robin Solanki <web.guru@me.com>
2020-04-29 13:33:55 +0530

    Configure Hamburger Menu animation

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -1,9 +1,16 @@
 import React from "react"
+
 // Styles
+import { HamburgerIcon } from "./Hamburger.styles"
+
 const Hamburger = ({ menuOpen, setMenuOpen }) => (
-  <div>
+  <HamburgerIcon
+    role="button"
+    menuOpen={menuOpen}
+    onClick={() => setMenuOpen(prev => !prev)}
+  >
     <div />
-  </div>
+  </HamburgerIcon>
 )
 
 export default Hamburger
