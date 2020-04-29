d0e21a06a8e0636f227712e0752d52c43df8627e
Robin Solanki <web.guru@me.com>
2020-04-29 15:35:54 +0530

    Configure a Blog Post component

--- src/components/BlogPostCard/index.js
+++ src/components/BlogPostCard/index.js
@@ -1,7 +1,36 @@
 import React from "react"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
 
-const BlogPostCard = () => {
-  return <div>Blog Post Card</div>
+// Icons
+import DateIcon from "images/calendar.svg"
+import TimeIcon from "images/watch.svg"
+
+const BlogPostCard = ({ slug, title, date, readingTime, excerpt, image }) => {
+  return (
+    <CardWrapper>
+      <ImgWrapper>
+        <Img fluid={image} />
+      </ImgWrapper>
+
+      <TextWrapper>
+        <Link to={slug}>
+          <h2>{title}</h2>
+        </Link>
+        <StatWrapper>
+          <DateWrapper>
+            <img src={DateIcon} alt="date" />
+            {date}
+          </DateWrapper>
+          <ReadingTimeWrapper>
+            <img src={TimeIcon} alt="time" />
+            {readingTime}
+          </ReadingTimeWrapper>
+        </StatWrapper>
+        <p>{excerpt}</p>
+      </TextWrapper>
+    </CardWrapper>
+  )
 }
 
 export default BlogPostCard
