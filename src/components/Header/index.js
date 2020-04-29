35338faf554481b5e8f0b0fc2de4f02a137d5d54
Robin Solanki <web.guru@me.com>
2020-04-29 16:32:09 +0530

    Implement Mode Button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import { Link } from "gatsby"
 
 // Components
 import Menu from "components/Menu"
 import Hamburger from "components/Hamburger"
 import MobileMenu from "components/MobileMenu"
+import ModeButton from "components/ModeButton"
 
 // Hooks
 import { useSiteConfigQuery } from "hooks/useSiteConfigQuery"
+
+// Context
+import { ModeContext } from "context/ModeProvider"
 
 // Styles
 import { Wrapper, Logo } from "./Header.styles"
 
 const Header = ({ siteTite = `` }) => {
   const siteConfig = useSiteConfigQuery()
+  const [darkMode, setDarkMode] = useContext(ModeContext)
 
   const [menuOpen, setMenuOpen] = useState(false)
 
@@ -25,7 +30,7 @@ const Header = ({ siteTite = `` }) => {
       <Link to="/">
         <Logo src={siteConfig.logo.publicURL} alt={siteTite} />
       </Link>
-      <div>Mode Button</div>
+      <ModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
     </Wrapper>
   )
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useContext } from "react"
 import { ThemeProvider } from "styled-components"
 
 // Components
 import Header from "components/Header"
+
+// Context
+import { ModeContext } from "context/ModeProvider"
 
 //Hooks
 import { useMetaDataQuery } from "hooks/useMetaDataQuery"
@@ -12,11 +15,10 @@ import { GlobalStyles, lightTheme, darkTheme } from "styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   const data = useMetaDataQuery()
-
-  console.log(data)
+  const [darkMode] = useContext(ModeContext)
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
       <Header siteTitle={data.title} />
       {children}
